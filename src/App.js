261e7a16c69b5bf487ede8c67349c3615156ed5d
Rohit Saluja <saluja730@gmail.com>
2023-09-08 11:25:52 +0530

    updated the prettier

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-        <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
+        <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
           custom
         </button>
       </header>
