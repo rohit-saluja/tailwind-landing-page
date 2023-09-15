35cc5c3c367f20c3722b390002807e2753c28eff
Rohit Saluja <saluja730@gmail.com>
2023-09-15 19:19:23 +0530

    updated the

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <h1 className='font-bold underline text-3xl sdfsdfsdff '>Hello world! </h1>;
+  return <h1 className='font-bold underline text-3xl  sdjflsadjf sdaj  '>Hello world! </h1>;
 }
 
 export default App;
