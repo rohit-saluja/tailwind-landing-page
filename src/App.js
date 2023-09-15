178ae0f0d72bfb0433a12da50099f8be65f5867e
Rohit Saluja <saluja730@gmail.com>
2023-09-15 19:11:55 +0530

    prettier is installed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="font-bold underline text-3xl ">Hello world! </h1>;
 }
 
 export default App;
