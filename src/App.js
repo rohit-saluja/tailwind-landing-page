d787c6b8ca2e26eb92020aa776becd5706937496
Rohit Saluja <saluja730@gmail.com>
2023-09-18 20:02:51 +0530

    updatged

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
 
 function App() {
-  return <h1 className='font-bold underline text-3xl'>Hello world! </h1>;
+  return (
+    <div className='flex h-52 w-full bg-blue-950'>
+      <div className='z-40 flex h-20 w-20 items-center justify-center rounded-full border-2 border-blue-950 bg-pink-600 text-white'>
+        05
+      </div>
+      <div className='border-1 z-30 -ml-6 flex h-20 w-20 items-center justify-center rounded-full border-2 border-blue-950 bg-pink-600 text-white'>
+        05
+      </div>
+      <div className='border-1 z-20 -ml-6 flex h-20 w-20 items-center justify-center rounded-full border-2 border-blue-950 bg-pink-600 text-white'>
+        05
+      </div>
+    </div>
+  );
 }
 
 export default App;
