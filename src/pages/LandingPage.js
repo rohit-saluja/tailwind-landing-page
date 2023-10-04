c41e725fb1d65d3f3d6c0c61aa015d55be04ad58
Rohit Saluja <saluja730@gmail.com>
2023-10-04 15:08:52 +0530

    updated the app

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 function LandingPage() {
   return (
     <div>
-      <section className="w-full bg-[url('./assets/images/background-image.png')]">
+      <section className="w-full bg-[url('./../public/background-image.png')]">
         <Navbar />
       </section>
     </div>
