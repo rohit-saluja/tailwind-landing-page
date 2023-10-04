2b68d60c42c270513a53bfc67aa36b33d00e20be
Rohit Saluja <saluja730@gmail.com>
2023-10-04 13:32:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className='flex h-52 w-full bg-blue-950'>
-      <div className='z-40 flex h-20 w-20 items-center justify-center rounded-full border-2 border-blue-950 bg-pink-600 text-white'>
-        05
-      </div>
-      <div className='border-1 z-30 -ml-6 flex h-20 w-20 items-center justify-center rounded-full border-2 border-blue-950 bg-pink-600 text-white'>
-        05
-      </div>
-      <div className='border-1 z-20 -ml-6 flex h-20 w-20 items-center justify-center rounded-full border-2 border-blue-950 bg-pink-600 text-white'>
-        05
+    <div className='container h-screen'>
+      <div className='m-6 flex justify-center gap-3'>
+        <input type='text' className='rounded-md border-2 border-red-500' />
+        <select name='' id=''>
+          <option value=''>select</option>
+          <option value=''>select</option>
+          <option value=''>select</option>
+        </select>
       </div>
     </div>
   );
