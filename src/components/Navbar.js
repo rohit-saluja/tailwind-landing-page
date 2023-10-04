09d937a1ee824818040ba015647983b2d890bf32
Rohit Saluja <saluja730@gmail.com>
2023-10-04 16:12:08 +0530

    added the header for the tailwind project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,23 @@
 function NavBar() {
   return (
     <div className='container mx-auto'>
-      <div className='flex justify-around py-5'>
+      <div className='flex items-center justify-around py-5'>
         <img src='logo-dark.png' alt='' />
         <div className='flex justify-between gap-7'>
           <span className='relative text-blue-500 after:absolute after:-bottom-1 after:left-1/2  after:w-5 after:-translate-x-1/2 after:border-b-2 after:border-blue-500 after:content-[""]'>
             Home
           </span>
           <span className='text-slate-500'>Features</span>
-          <span>Screenshots</span>
-          <span>Testimonial</span>
-          <span>Pricing</span>
-          <span>Contact us</span>
+          <span className='text-slate-500'>Screenshots</span>
+          <span className='text-slate-500'>Testimonial</span>
+          <span className='text-slate-500'>Pricing</span>
+          <span className='text-slate-500'>Contact us</span>
         </div>
-        <div className='flex gap-2'>
-          <span>Login</span>
-          <span>Signup</span>
+        <div className='flex items-center gap-2'>
+          <span className='text-slate-500'>Login</span>
+          <span className='rounded-sm border border-gray-300 bg-blue-200 px-3 py-1 text-blue-500'>
+            Sign Up
+          </span>
         </div>
       </div>
     </div>
